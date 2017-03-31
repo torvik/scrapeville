233359400a05073dceed871505a398e68ade4fd6
torvik <pless.victor@gmail.com>
2017-03-31 02:09:01 +0000

    added style2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
+    @newsvilles = Newsville.all.first(4)
+
+    @comments = Comment.all.first(30)
+
   end
 end
--- gp
+++ gp
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "added style"
+git commit -m "added style2"
 git push -u origin master
 git push heroku master
