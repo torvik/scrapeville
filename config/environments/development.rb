7e4ad0fe291a1c4962335904bad9f084b07efd1d
torvik <pless.victor@gmail.com>
2017-04-04 23:56:34 +0000

    atualizacao aws configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,12 +56,14 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    :region => ENV['AWS_REGION'],
+    #:region => ENV['AWS_REGION'],
+    s3_region: 'us-west-2',
     s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
+      s3_host_name: "s3-us-west-2.amazonaws.com"
+      #s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
     }
   }
 
--- gp
+++ gp
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "atualizacao aws"
+git commit -m "atualizacao aws configs"
 git push -u origin master
 git push heroku master
