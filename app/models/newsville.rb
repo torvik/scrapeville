edd5a8d845e7b61cbafafb447056d30ac4b91000
torvik <pless.victor@gmail.com>
2017-04-05 00:54:44 +0000

    loading

--- app/models/newsville.rb
+++ app/models/newsville.rb
@@ -3,5 +3,6 @@ class Newsville < ApplicationRecord
   belongs_to :user
   has_many :comments
 
+  validates :title_url, uniqueness: true
 
 end
--- gp
+++ gp
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "image"
+git commit -m "loading"
 git push -u origin master
 git push heroku master
